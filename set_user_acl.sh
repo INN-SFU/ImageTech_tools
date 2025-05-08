0b74dad40e6c7766de256c3323a12722a20051ea
lrokos <lrokos@rcg-imgtech-2.research.sfu.ca>
2025-05-07 17:15:40 -0700

    fix(paths): change default directory paths

--- set_user_acl.sh
+++ set_user_acl.sh
@@ -39,8 +39,8 @@
 ##############################################################################
 
 # Hardcoded directories
-PREFIX_DIR="/project/ctb-rmcintos/testing_permissions"
-USER_FACLS_DIR="./user_facls"  # Directory where all user ACL files are stored
+PREFIX_DIR="/data/storage/"
+USER_FACLS_DIR="/data/storage/software/user_facls"  # Directory where all user ACL files are stored
 
 # Ensure the ACL directory exists
 if [[ ! -d "$USER_FACLS_DIR" ]]; then
